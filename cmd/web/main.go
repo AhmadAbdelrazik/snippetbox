e6d3d8b03bff242ecabcb5abc2707bc6138649a2
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2024-07-09 14:13:39 +0300

    Added Routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,45 +1,62 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	errLogger  *log.Logger
-	infoLogger *log.Logger
+	errorLog *log.Logger
+	infoLog  *log.Logger
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTTP network address")
-
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ltime|log.Ldate)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	app := &application{
-		infoLogger: infoLog,
-		errLogger: errorLog,
-	}
-
 	flag.Parse()
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreate)
+	db, err := openDB(*dsn)
+
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
 
 	srv := &http.Server{
-		Addr:    *addr,
-		Handler: mux,
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Stating server on %s", *addr)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
